9f50e940af7ec78e7c7d863f079aaed9acca58c7
Andrey Belyaev <svetomech@gmail.com>
2014-12-15 17:57:24 +0300

    Normalize endings?

--- filename_coder.cs
+++ filename_coder.cs
@@ -11,7 +11,7 @@ class FilenameCoder
 	{
 		MessageBox.Show("No passed argument or is not a file.",
 		                app_title,
-						MessageBoxButtons.OK);
+		                MessageBoxButtons.OK);
 		Environment.Exit(0);
 	}
 
@@ -30,7 +30,7 @@ class FilenameCoder
 		string file_name_coded;
 		var btn_clicked = MessageBox.Show("Do you want to decode the file (Yes) or encode it (No)?",
 		                                  app_title,
-										  MessageBoxButtons.YesNoCancel);
+		                                  MessageBoxButtons.YesNoCancel);
 		if (btn_clicked == DialogResult.Yes)
 			file_name_coded = Uri.UnescapeDataString(file_name) + file_ext;
 		else if (btn_clicked == DialogResult.No)
